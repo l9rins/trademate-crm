1b931e3de796a4e8cb454f352daf7b10bf218146
Marquee <lorenzbarangan112@gmail.com>
2026-02-05 02:45:15 +0800

    fix(deploy): rewrite vite.config.js and remove cache

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -15,4 +14,3 @@ export default defineConfig({
     },
   },
 })
-
